2c654a97319225dd49e5c49d67dd790e5aa7ac4e
leminhhuan72 <leminhhuan72@gmail.com>
2021-11-15 23:17:36 +0700

    cGame class and cPeople class

--- mini_game/cPeople.h
+++ mini_game/cPeople.h
@@ -1,1 +1,19 @@
-#pragma once
+﻿#ifndef _PEOPLE_H_
+#define _PEOPLE_H_
+
+class CPEOPLE {
+	int mX, mY;
+	bool mState; //Trạng thái sống chết
+public:
+	CPEOPLE();
+	void Up(int);
+	void Left(int);
+	void Right(int);
+	void Down(int);
+	bool isImpact(const CVEHICLE*&);
+	bool isImpact(const CANIMAL*&);
+	bool isFinish();
+	bool isDead();
+};
+
+#endif // !1
