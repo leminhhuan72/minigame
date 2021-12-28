a073e78b3084888b6b18653dcbc55de3defc22da
leminhhuan72 <leminhuan72@gmail.com>
2021-12-28 22:12:13 +0700

    background sounds

--- MiniPJGame/SoundControl.cpp
+++ MiniPJGame/SoundControl.cpp
@@ -1,28 +1,8 @@
 #include "SoundControl.h"
 
-void SoundControl::playBackGround1(){
-    playBackGround("Sound//Background.wav");
-}
-void SoundControl::playBackGround2(){
-    playBackGround("Sound//Background2.wav");
-}
-
-void SoundControl::playSound(string s){
+void SoundControl::playBackGround(string s){
+    if (s==curBackGround) return;
     if (isPlaySound)
-        PlaySound(s.c_str(), NULL, SND_SYNC);
+        PlaySound(s.c_str(), NULL, SND_ASYNC | SND_LOOP);
     curBackGround = s;
-}
-
-void SoundControl::changeStatus(){
-    if (isPlaySound){
-        string tmp = curBackGround;
-        playSound("Sound//move.wav");
-        isPlaySound = false;
-        curBackGround = tmp;
-    }else{
-        isPlaySound = true;
-        string tmp = curBackGround;
-        curBackGround = "";
-        playBackGround(tmp);
-    }
 }
