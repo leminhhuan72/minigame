38101de4fd8af59c236706545d9088d01f502230
leminhhuan72 <leminhuan72@gmail.com>
2021-12-29 22:16:48 +0700

    fix console and gotoXY methos in window control

--- MiniPJGame/windowControl.cpp
+++ MiniPJGame/windowControl.cpp
@@ -1,1 +1,15 @@
 #include "windowControl.h"
+void FixConsoleWindow() {
+    ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+    HWND consoleWindow = GetConsoleWindow();
+    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+    style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+    SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void GotoXY(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
