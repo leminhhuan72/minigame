f53caa9acf89f8470ca5abe1d0267651ab8b8419
leminhhuan72 <leminhuan72@gmail.com>
2022-01-05 22:13:42 +0700

    fix change status in sound control

--- MiniPJGame/SoundControl.cpp
+++ MiniPJGame/SoundControl.cpp
@@ -19,3 +19,17 @@ void SoundControl::playSound(string s){
         PlaySound(s.c_str(), NULL, SND_SYNC);
     curBackGround = s;
 }
+
+void SoundControl::changeStatus(){
+    if (isPlaySound){
+        string tmp = curBackGround;
+        playSound("Sound//move.wav");
+        isPlaySound = false;
+        curBackGround = tmp;
+    }else{
+        isPlaySound = true;
+        string tmp = curBackGround;
+        curBackGround = "";
+        playBackGround(tmp);
+    }
+}
